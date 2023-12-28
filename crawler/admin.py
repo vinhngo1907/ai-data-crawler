2db50892800e9b987800cac54464f00fb46fec3c
Henry Ngo <henry.ngo@lizai.co>
2023-12-28 15:18:03 +0700

    بدعت حبيت الغنبه♥♡♥♡♥♡متناسق وي الفيدرو أبداع

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from .models import UserProfile, KeyWord, Category, Link, CrawledLinks, SocialMedia
+from .models import (
+    UserProfile,
+    KeyWord,
+    Notifications,
+    Category,
+    Link,
+    CrawledLinks,
+    SocialMedia,
+)
+
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'crawled_links', 'scraped_data')
-    search_fields = ['user']
+    list_display = ("user", "crawled_links", "scraped_data")
+    search_fields = ["user"]
+
+
+class NotificationsAdmin("user", "pub_date"):
+    list_display = ("user", "pub_date")
+    search_fields = ["user"]
+
+    class Meta:
+        verbose_name_plural = "Notifications"
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Notifications, NotificationsAdmin)
