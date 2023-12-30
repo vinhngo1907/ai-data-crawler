6b158e081611a5c4a1e635a633cc3a26d606344a
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-12-30 17:25:13 +0700

    ❤👍😎

--- utils/analytics.py
+++ utils/analytics.py
@@ -3,7 +3,7 @@ from pytrends.request import TrendReq
 import random
 
 
-def category_percent(user):
+def category_count(user):
     temp = list()
     categories = Category.objects.all()
     userprofile = UserProfile.objects.get(user=user)
@@ -16,4 +16,48 @@ def category_percent(user):
         )
 
         temp.append(links)
+
     return temp
+
+
+def category_percent(user):
+    temp = category_count(user)
+    total = sum(temp)
+    percent = list()
+    context = dict()
+
+    for item in temp:
+        if total != 0:
+            percent.append((item / total) * 100)
+
+    categories = Category.objects.all()
+    for category, p in zip(categories, percent):
+        context[category.name] = round(p, 2)
+
+    return context
+
+
+def keyword_trend(keyword_list):
+    context = dict()
+    temp = list()
+    try:
+        print("Hello")
+        pytrend = TrendReq(timeout=(10, 25), backoff_factor=0.5)
+
+        def color():
+            return random.randint(0, 255)
+
+        pytrend.build_payload(kw_list=keyword_list)
+        interest_over_time_df = pytrend.interest_over_time()
+        labels = interest_over_time_df.index.tolist()
+        for item in labels:
+            temp.append(str(item))
+
+        for keyword in keyword_list:
+            context[keyword] = interest_over_time_df[keyword].tolist(), (
+                "#%02X%02X%02X" % (color(), color(), color())
+            )
+    except:
+        print(" No Trends")
+
+    return temp, context
