47ba3a48e1077d98181a91a225e9d5d9864edecf
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-12-30 18:23:41 +0700

    🥰

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,27 +1,81 @@
 from django.contrib import admin
 from .models import (
     UserProfile,
-    KeyWord,
     Notifications,
+    Keyword,
     Category,
     Link,
     CrawledLinks,
     SocialMedia,
 )
 
+admin.site.site_header = "Falcon administration"
+
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "crawled_links", "scraped_data")
+    list_display = (
+        "user",
+        "crawled_links",
+        "scraped_data",
+    )
     search_fields = ["user"]
 
 
-class NotificationsAdmin("user", "pub_date"):
-    list_display = ("user", "pub_date")
+class NotificationsAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "pub_date",
+    )
     search_fields = ["user"]
 
     class Meta:
         verbose_name_plural = "Notifications"
 
 
+class SocialMediaAdmin(admin.ModelAdmin):
+    list_display = (
+        "keyword",
+        "timestamp",
+    )
+    search_fields = ["keyword"]
+    list_filter = ["keyword", "timestamp"]
+
+    class Meta:
+        verbose_name_plural = "Social Media"
+
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = (
+        "keyword",
+        "category",
+        "link",
+    )
+    search_fields = ["keyword", "category"]
+    list_filter = ["keyword", "category"]
+
+
+class CrawledLinksAdmin(admin.ModelAdmin):
+    list_display = (
+        "userprofile",
+        "reschedule",
+        "link",
+    )
+    search_fields = ["userprofile", "link__keyword__name", "link__category__name"]
+    list_filter = [
+        "userprofile",
+        "link__category",
+        "link__keyword__name",
+    ]
+    list_editable = ("reschedule",)
+
+
+# admin.site.unregister(auth.models.Group)
+# admin.site.unregister(auth.models.User)
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Notifications, NotificationsAdmin)
+
+admin.site.register(Keyword)
+admin.site.register(Category)
+admin.site.register(Link, LinkAdmin)
+admin.site.register(CrawledLinks, CrawledLinksAdmin)
+admin.site.register(SocialMedia, SocialMediaAdmin)
