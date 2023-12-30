4fc2e48fd42d7e52d8a68637ff4c8691bf7c49af
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-12-30 10:29:31 +0700

    manager

--- manager.py
+++ manager.py
@@ -1,14 +1,19 @@
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'falcon.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "falcon.settings")
     try:
-        print("Hello World")
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Coun't import Django"
-        )
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
