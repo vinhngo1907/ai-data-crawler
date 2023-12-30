80d1963f64c93b3fef96a594b82035fb9c19880a
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-12-30 17:54:29 +0700

    😂😂😂

--- crawler/urls.py
+++ crawler/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 app_name = "crawler"
 
 urlpatterns = [
-    path("/dashborad", views.index, name="index"),
+    path("dashborad/", views.index, name="index"),
     path("crawler/", views.crawler_index, name="crawler_index"),
 ]
