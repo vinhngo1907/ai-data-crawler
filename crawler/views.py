bab2497afb71d79fcabdd2586f36ad1230b09e4e
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-12-30 17:27:23 +0700

    💃🏽🕺🏽🍻

--- crawler/views.py
+++ crawler/views.py
@@ -6,20 +6,24 @@ from django.db.models import Count
 from django.contrib import messages
 from utils.analytics import category_percent
 
+
 @login_required
 def index(request):
-    """
-    """
+    """ """
     userprofile = UserProfile.objects.get_or_create(user=request.user)[0]
     category = Category.objects.all()
-    notifications = Notifications.objects.filter(user=userprofile).order_by('-pub_date')
+    notifications = Notifications.objects.filter(user=userprofile).order_by("-pub_date")
     unread = notifications.filter(read=False)
-    user_crawled_links = CrawledLinks.objects.filter(userprofile=userprofile).order_by('-pub_date')
+    user_crawled_links = CrawledLinks.objects.filter(userprofile=userprofile).order_by(
+        "-pub_date"
+    )
     context = dict()
-    context['home'] = True
-    context['category'] = category
-    context['userprofile'] = userprofile
-    context['notifications'] = notifications[5]
-    context['unread_count'] = len(unread)
-    context['user_crawled'] = user_crawled_links[:userprofile.recent_link]
-    context['cat_percent'] = category_percent()
+    context["home"] = True
+    context["category"] = category
+    context["userprofile"] = userprofile
+    context["notifications"] = notifications[5]
+    context["unread_count"] = len(unread)
+    context["user_crawled"] = user_crawled_links[: userprofile.recent_link]
+    context["cat_percent"] = category_percent(request.user)
+
+    return render(request, "crawler/index.html", context=context)
