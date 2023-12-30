8f700d87d0c25c9bae8fd48cd67953042fe0374d
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-12-30 10:41:31 +0700

    commit

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
-from crawler.models import UserProfile, Notifications
+from crawler.models import UserProfile, Notifications, Category
+# from scheudler.models import ScrapedLink
+
+from django.contrib import messages
+# from utils.
+
+@login_required
+def index(request):
+    """
+    """
+    userprofile = UserProfile.objects.get_or_create(user=request.user)[0]
+    category = Category.objects.all()
+    notifications = Notifications.objects.filter()
