f069340f2a648f742f0f851694d612bf5026fd9d
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-12-31 21:08:32 +0700

    commit

--- crawler/urls.py
+++ crawler/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("dashboard/", views.index, name="index"),
     path("crawler/", views.crawler_index, name="crawler_index"),
     path("report/", views.report, name="report"),
-    # path("test/", views.test, name="test"),
-    # path("calendar/", views.calendar, name="calendar"),
+    path("test/", views.test, name="test"),
+    path("calendar/", views.calendar, name="calendar"),
+    path("social-scrape-data/", views.social, name="social"),
 ]
--- utils/crawler_spider.py
+++ utils/crawler_spider.py
@@ -26,19 +26,78 @@ def google_query(query, **kwargs):
 
 keywords = ["Crime", "Child Abuse", "Women Abuse", "Cyber Bullying"]
 
+# def crawling(query, keyword):
+#     """
+#     :param query:
+#     :param keywords:
+#     :return: crawled links, scrape data
+#     """
+#     general = list()
+#     crime = list()
+#     child_abuse = list()
+#     women_abuse = list()
+#     pornography = list()
+#     rape = list()
+#     cyber_bullying = list()
+#     general_scrape = list()
+#     crime_scrape = list()
+#     child_abuse_scrape = list()
+#     women_abuse_scrape = list()
+#     pornography_scrape = list()
+#     rape_scrape = list()
+#     cyber_bullying_scrape = list()
+
+#     contenxt = {
+#         "general": general,
+#         "crime": crime,
+#         "child abuse": child_abuse,
+#         "woman abuse": women_abuse,
+#         "cyber_bullying": cyber_bullying,
+#     }
+#     reduced = [
+#         general[:4],
+#         crime[:4],
+#         child_abuse[:4],
+#         women_abuse[:4],
+#         cyber_bullying[:4],
+#     ]
+
+#     reduced2 = [
+#         general_scrape[:4],
+#         crime_scrape[:4],
+#         child_abuse_scrape[:4],
+#         women_abuse_scrape[:4],
+#         cyber_bullying_scrape[:4],
+#     ]
+
+
+def scraper(link):
+    """
+
+    :param link:
+    :return: scrape data
+    """
+    print(f"scraping {link}")
+    if "youtube" in str(link):
+        return {}
+    else:
+        metadata = dict()
+        return metadata
+
 
 def social_media_scrape(keyword):
     result = {}
-    temp = ("twitter.com",)
+    temp = {}
+    base_url = "twitter.com"
     query = str(keyword) + " " + keywords[0]
-    tweets = query_tweets(query, limit=1, begindate=dt.date(2023, 6, 21))
+    tweets = query_tweets(query, limit=1, begindate=dt.date(2020, 6, 21))
     return tweets
 
 
 def extract_image(data):
-    print("Extracting images", data)
+    print("exttracting images", data)
     try:
-        image = data["pagemap"]["metags"][0]["og:image"]
+        image = data["pagemap"]["metatags"][0]["og:image"]
         return image
     except:
         pass
@@ -48,49 +107,6 @@ def create_predict_image(images, images_list):
     context = dict()
     for image in images:
         for image_list in images_list:
-            context[image] = images_list
+            context[image] = image_list
 
-
-def crawling(query, keyword):
-    """
-    :param query:
-    :param keywords:
-    :return: crawled links, scrape data
-    """
-    general = list()
-    crime = list()
-    child_abuse = list()
-    women_abuse = list()
-    pornography = list()
-    rape = list()
-    cyber_bullying = list()
-    general_scrape = list()
-    crime_scrape = list()
-    child_abuse_scrape = list()
-    women_abuse_scrape = list()
-    pornography_scrape = list()
-    rape_scrape = list()
-    cyber_bullying_scrape = list()
-
-    contenxt = {
-        "general": general,
-        "crime": crime,
-        "child abuse": child_abuse,
-        "woman abuse": women_abuse,
-        "cyber_bullying": cyber_bullying,
-    }
-    reduced = [
-        general[:4],
-        crime[:4],
-        child_abuse[:4],
-        women_abuse[:4],
-        cyber_bullying[:4],
-    ]
-
-    reduced2 = [
-        general_scrape[:4],
-        crime_scrape[:4],
-        child_abuse_scrape[:4],
-        women_abuse_scrape[:4],
-        cyber_bullying_scrape[:4],
-    ]
+    return context
